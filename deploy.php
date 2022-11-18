de20831b72d66e7726df2414153c7e8f3f4df076
mpraeauer <mpraeauer.mmt-b2021@fh-salzburg.ac.at>
2022-11-18 22:12:42 +0100

    Aaron WP

---
+++ deploy.php
@@ -0,0 +1,62 @@
+<?php
+namespace Deployer;
+
+require 'recipe/wordpress.php';
+
+set('repository','https://github.com/mpraeauer/wpaaron_Konstantin_Marlene');
+
+
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+
+
+// Shared files/dirs between deploys
+set('shared_files', ['public/wp-config.php']);
+set('shared_dirs', ['public/wp-content/uploads']);
+
+
+
+// Writable dirs by web server
+// set('writable_mode', 'chown');
+// set('writable_dirs', ['public/wp-content/uploads']);
+set('allow_anonymous_stats', false);
+
+
+
+// Hosts
+host('193.170.119.200')
+        ->set('remote_user','admin')
+        ->set('become', 'root')
+        ->set('port','5412')
+        ->set('deploy_path', '/home/admin/aaron');
+
+
+
+
+// Composer
+set('composer_action', false);
+
+
+
+
+// Tasks
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+]);
+
+
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');

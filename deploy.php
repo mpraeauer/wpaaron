17004f657bce15745c5fb32bd2dce741a8c6b104
mpraeauer <mpraeauer.mmt-b2021@fh-salzburg.ac.at>
2022-11-19 13:07:29 +0100

    richtige deploy.php

--- deploy.php
+++ deploy.php
@@ -1,11 +1,11 @@
 <?php
 namespace Deployer;
-require 'recipe/wordpress.php';
+require 'recipe/common.php';
 
 set('repository','git@github.com:mpraeauer/wpaaron_Konstantin_Marlene.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+//set('git_tty', true);
 // Shared files/dirs between deploys
 
 set('shared_files', ['public/wp-config.php', 'public/.htaccess']);
@@ -14,7 +14,7 @@ set('shared_dirs', ['public/wp-content/uploads']);
 // Writable dirs by web server
 // set('writable_mode', 'chown');
 // set('writable_dirs', ['public/wp-content/uploads']);
-set('allow_anonymous_stats', false);
+//set('allow_anonymous_stats', false);
 
 
 
@@ -22,7 +22,7 @@ set('allow_anonymous_stats', false);
 host('vm-copper.multimediatechnology.at')
         ->set('remote_user','admin')
         //->set('remote_user','aaron')
-         ->set('become', 'root')
+        // ->set('become', 'root')
         ->set('port','5412')
         ->set('deploy_path', '/home/admin/aaron');
         //->set('deploy_path', '~/app');
@@ -33,17 +33,17 @@ set('composer_action', false);
 // Tasks
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
+    //'deploy:info',
     'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    // 'deploy:publish'
+    //'deploy:lock',
+    //'deploy:release',
+   // 'deploy:update_code',
+    //'deploy:shared',
+    //'deploy:writable',
+    //'deploy:clear_paths',
+    //'deploy:symlink',
+    //'deploy:unlock',
+     'deploy:publish'
 ]);
 
 

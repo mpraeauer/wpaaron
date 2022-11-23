c96cb08a61da10e8ef1790d2189ecee7f0df9c83
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2022-11-23 11:10:40 +0100

    fixed title

--- src/php/index.php
+++ src/php/index.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo get_the_title() ?></title>
+    <title>Aaron G. Miller | Choreographer </title>
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/images/logo.png">
     <?php wp_head(); ?>
 </head>
